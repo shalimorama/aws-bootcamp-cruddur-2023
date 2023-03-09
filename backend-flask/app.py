64d409be8580399bf7bbfb5dd14484a152bfc03d
shalimorama <shalimorama@gmail.com>
2023-03-09 16:21:40 +0000

    before server side verification

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -80,8 +80,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -154,6 +154,7 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run(logger=LOGGER)
+
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
